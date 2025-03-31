eebcb8ab15d6cc86c4665854631f03ce1ff1284d
lea <lea.kruse2010@gmail.com>
2025-03-31 13:00:31 +0200

    Add comments to config

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
+# Whether to print debug messages
 debug = False
 
+# Header for fetching album cover art
 musicbrainzHeaders = {
     "User-Agent": "MPRIS2_SAVER/0.0.1"
 }
@@ -7,5 +9,7 @@ musicbrainzHeaders = {
 # Refresh rate of script, can be used to reduce performance hit
 rateInterval = 2.5
 
+# Target mpris player to use
 playerTarget = "org.mpris.MediaPlayer2.spotify"
+# Target pipewire node to record
 pwTarget = "spotify"
